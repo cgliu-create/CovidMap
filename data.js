364bfcfc36d5ba681c6bf25983ab06ab43fb9edb
cgliu-create <chrisliu2438@gmail.com>
2021-08-06 22:03:59 -0400

    colors

--- data.js
+++ data.js
@@ -25,8 +25,48 @@ function update() {
             states[i] = states[min];
             states[min] = copy;
         }
-
+        let colors =[
+            "hsl(0, 100%, 95%)",
+            "hsl(0, 100%, 90%)",
+            "hsl(0, 100%, 85%)",
+            "hsl(0, 100%, 80%)",
+            "hsl(0, 100%, 75%)",
+            "hsl(0, 100%, 70%)",
+            "hsl(0, 100%, 65%)",
+            "hsl(0, 100%, 60%)",
+            "hsl(0, 100%, 55%)",
+            "hsl(0, 100%, 50%)",
+            "hsl(0, 100%, 45%)",
+            "hsl(0, 100%, 40%)",
+            "hsl(0, 100%, 35%)",
+            "hsl(0, 100%, 30%)",
+            "hsl(0, 100%, 25%)",
+            "hsl(0, 100%, 20%)",
+            "hsl(0, 100%, 15%)",
+            "hsl(0, 100%, 10%)",
+            "hsl(0, 100%, 5%)",
+            "hsl(0, 100%, 0%)"
+        ]
         console.log(states)
+        console.log(nums)
+       
+        let color = 1;
+        let counter = 1;
+
+        for (const state of states) {
+            let tag = state.toLowerCase();
+            console.log(tag);
+            let el = document.getElementsByClassName(tag);
+            for (const l of el) {
+                l.style.fill = colors[color];
+                console.log(colors[color])
+            }
+            counter +=1;
+            if (counter == 4) {
+                color +=1
+                counter = 1;
+            }
+        }
 
       }).catch(function() {
         console.log("Booo");
